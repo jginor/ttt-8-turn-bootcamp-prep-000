85bf10a2c056e76bca6967273430646cc7d38ccd
Jordan Ginor <jordan.ginor@gmail.com>
2018-12-03 01:21:32 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,8 @@
+def display_board(board)
+board [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+puts "{board[1]|#oard{2}|#board{3}"
+puts "-----------"
+puts "#board{4}|#board{5}|#board{6}"
+puts "-----------"
+puts "#board{7}|#board{8}|#(board{9})"
+end
